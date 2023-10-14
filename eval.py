fddb090b1d9fe87edc51058dee7072537478278b
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-10-13 22:56:51 -0500

    add download to test data

--- eval.py
+++ eval.py
@@ -88,8 +88,7 @@ def main():
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
         ])
-    dataset2 = datasets.MNIST('data', train=False,
-                       transform=transform)
+    dataset2 = datasets.MNIST('data', train=False, download=True, transform=transform)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = Net()
