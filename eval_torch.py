5ab1c28532d6b9a141203119a3a8d9892b0a0a93
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-10-15 20:53:37 -0500

    simplify eval

--- eval_torch.py
+++ eval_torch.py
@@ -1,87 +1,42 @@
-from __future__ import print_function
-import argparse
 import time
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.optim.lr_scheduler import StepLR
+import numpy as np
 from torchinfo import summary
 
 from train_torch import Net
 
-def test(model, device, test_loader):
+def test(model, device, data, target):
     model.eval()
-    test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    print(f"Accuracy: {correct}/{1000} ({100.*correct/1000:.0f}%)")
 
 
 def main():
-    # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--no-mps', action='store_true', default=False,
-                        help='disables macOS GPU training')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    use_mps = not args.no_mps and torch.backends.mps.is_available()
+    device = "cpu"
 
-    torch.manual_seed(args.seed)
+    # Load data
+    npzfile = np.load("data/mnist_test_1000.npz")
+    data = torch.tensor(npzfile["data"])
+    target = torch.tensor(npzfile["target"])
 
-    if use_cuda:
-        device = torch.device("cuda")
-        print(f"{device}: {torch.cuda.get_device_name(0)}")
-    elif use_mps:
-        device = torch.device("mps")
-    else:
-        device = torch.device("cpu")
-        print(f"{device}: # of threads {torch.get_num_threads()}")
-
-    test_kwargs = {'batch_size': args.test_batch_size}
-    if use_cuda:
-        cuda_kwargs = {'num_workers': 1,
-                       'pin_memory': True,
-                       'shuffle': True}
-        test_kwargs.update(cuda_kwargs)
-
-    transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-        ])
-    dataset2 = datasets.MNIST('data', train=False, download=True, transform=transform)
-    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
-
+    # Load model
     model = Net()
     model.load_state_dict(torch.load("mnist_cnn.pt", map_location="cpu"))
-    summary(model, input_size=(args.test_batch_size, 1, 28, 28))
+    summary(model, input_size=(1000, 1, 28, 28))
 
     model.to(device)
     model = torch.jit.script(model)
 
     start = time.time()
-    test(model, device, test_loader)
+    test(model, device, data, target)
     end = time.time()
-
-    print(f"{end - start:.4f} seconds")
+    print(f"Time: {end - start:.4f} seconds")
 
 if __name__ == '__main__':
     main()
